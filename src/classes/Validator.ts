93564a11fddc01895f377f6946dca4a88f37c730
Thiago Baptista <thiagobapt2003@gmail.com>
2024-09-17 17:01:01 -0300

    Added support for brackets, validator is done and working as expected

--- src/classes/Validator.ts
+++ src/classes/Validator.ts
@@ -2,17 +2,51 @@ export default class Validator {
 
     Validate(input: string): boolean {
 
-        let lastToken = [];
+        let tokens: string[] = [];
 
         const numberRegEx = new RegExp("^[0-9]$");
+        const operatorRegEx = new RegExp("^[*+=/]$");
+        const bracketRegEx = new RegExp("^[()]$");
 
         for (let index = 0; index < input.length; index++) {
             const char = input.charAt(index);
             
-            if(numberRegEx.test(char)) lastToken.push(char);
-
-            return false;
+            if(numberRegEx.test(char)) {
+                const lastIndex = tokens.length - 1;
+                if(numberRegEx.test(tokens[lastIndex])) {
+                    tokens[lastIndex] = tokens[lastIndex].concat(char);
+                } else {
+                    tokens.push(char);
+                }
+            }
+            else if(operatorRegEx.test(char)) {
+                tokens.push(char);
+            }
+            else if(bracketRegEx.test(char)) {
+                tokens.push(char);
+            } else {
+                return false;
+            }
         }
+
+        let openedBrackets = 0;
+        let closedBrackets = 0;
+
+        for (let index = 0; index < tokens.length; index++) {
+            const token = tokens[index];
+            if((index === 0 || index === tokens.length - 1) && operatorRegEx.test(token)) return false;
+            if(operatorRegEx.test(token) && operatorRegEx.test(tokens[index - 1])) return false;
+            
+            if(operatorRegEx.test(token) && bracketRegEx.test(tokens[index - 1])) return false;
+            if(operatorRegEx.test(tokens[index - 1]) && token === ')') return false;
+
+            if(token === '(') openedBrackets += 1;
+            if(token === ')') closedBrackets += 1;
+        }
+
+        if(openedBrackets != closedBrackets) return false;
+
+        return true;
     }
 
 }
